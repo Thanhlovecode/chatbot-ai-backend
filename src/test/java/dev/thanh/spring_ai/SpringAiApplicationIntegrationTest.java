7f6079afc7bbe3973734bc24ad6239f355b7851b
Thanh <Thanhlovecode@users.noreply.github.com>
2026-04-17 23:16:31 +0700

    fix(test): rename integration test classes to match *IntegrationTest filter pattern

--- src/test/java/dev/thanh/spring_ai/SpringAiApplicationIntegrationTest.java
+++ src/test/java/dev/thanh/spring_ai/SpringAiApplicationIntegrationTest.java
@@ -3,7 +3,7 @@ package dev.thanh.spring_ai;
 import org.junit.jupiter.api.Test;
 import dev.thanh.spring_ai.config.AbstractIntegrationTest;
 
-class SpringAiApplicationTests extends AbstractIntegrationTest {
+class SpringAiApplicationIntegrationTest extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
